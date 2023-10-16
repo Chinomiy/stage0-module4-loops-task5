225570839c424b2ff32607e23ca03cb29ee78fc1
Chinomiy <maruishiwo@gmail.com>
2023-10-16 16:15:07 +0300

    v1

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -1,7 +1,19 @@
 package school.mjc.stage0.loops.task5;
 
 public class Cross {
-    public void printCross(int sideLength) {
-
+    public static void main(String[] args) {
+        printCross(8);
+    }
+    public static void printCross(int sideLength) {
+        for (int x = 0; x < sideLength; x++) {
+            for (int y = 0; y < sideLength; y++) {
+                if (x == sideLength /2 || y == sideLength / 2  ) {
+                    System.out.print(8);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
